e5a3e333ec5d69d13b70454c34eca96c4464d3df
Freitas <adavidaf2002@gmail.com>
2024-12-08 11:54:31 +0000

    echo command fixed

--- app/main.c
+++ app/main.c
@@ -2,6 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+void ft_strscpy(char *src, char *dst, int n) {
+	char *srcs = src + n;
+	strcpy(dst, srcs);
+
+}
+
 int main() {
   
   char input[100];
@@ -18,7 +24,9 @@ int main() {
 		exit(0);
 	}
 	else if (!strncmp(input, "echo ", 5)) {
-		printf("%s\n", input);
+		char echo[95];
+		ft_strscpy(input, echo, 5);
+		printf("%s\n", echo);
 	}
 	else {
 		printf("%s: not found\n", input);

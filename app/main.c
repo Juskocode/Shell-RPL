5b311065771dc53dd6b5953b0ab3e5ac6e41eb2c
Freitas <adavidaf2002@gmail.com>
2024-12-08 10:05:35 +0000

    user input

--- app/main.c
+++ app/main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 int main() {
+  
   printf("$ ");
   fflush(stdout);
 
   char input[100];
   if (fgets(input, 100, stdin) != NULL) {
-	printf("%s", input);
+	printf("$ %s", input);
   }
   else {
-	perror("invalid input");
+	perror("$ invalid input");
   }
   return 0;
 }

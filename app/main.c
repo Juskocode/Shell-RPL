5f5acc2d89b1c7318ede0e37341d91e0c316da1f
Freitas <adavidaf2002@gmail.com>
2024-12-08 11:14:03 +0000

    REPL form

--- app/main.c
+++ app/main.c
@@ -8,11 +8,11 @@ int main() {
   printf("$ ");
   fflush(stdout);
 
-
   while (fgets(input, 100, stdin) != NULL) {
 	input[strlen(input) - 1] = '\0';	
 	printf("%s: not found\n", input);
-	printf("$ ");
+	printf("$ "); 
+  	fflush(stdout);
   }
   return 0;
 }

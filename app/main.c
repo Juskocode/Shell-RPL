8d7ae796d5693d275f4fa8151acc7b006f5f7af7
Freitas <adavidaf2002@gmail.com>
2024-12-08 10:28:14 +0000

    REPL form

--- app/main.c
+++ app/main.c
@@ -3,16 +3,15 @@
 
 int main() {
   
+  char input[100];
+
   printf("$ ");
   fflush(stdout);
 
-  char input[100];
-  if (fgets(input, 100, stdin) != NULL) {
+  while (fgets(input, 100, stdin) != NULL) {
 	input[strlen(input) - 1] = '\0';	
 	printf("%s: not found\n", input);
-  }
-  else {
-	perror("invalid input");
+	printf("$ ");
   }
   return 0;
 }

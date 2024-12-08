3e52ef5b5ee20f7802bad9ded99362545a4bd0b5
Freitas <adavidaf2002@gmail.com>
2024-12-08 13:25:34 +0000

    added const type safety, added type PATH search

--- app/main.c
+++ app/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define RESET   "\x1b[0m"
 #define GREEN   "\x1b[32m"
@@ -8,11 +9,39 @@
 #define YELLOW  "\x1b[33m"
 #define RED     "\x1b[31m"
 
-void execute_echo(char *input) {
+int check_exec(const char *path) {
+	return access(path, X_OK) == 0;
+}
+
+char* handle_path(const char *file) {
+	char *path_env = getenv("PATH");
+	if (!path_env)
+		return NULL;
+
+	char *path_copy = strdup(path_env);
+	char *dir = strtok(path_copy, ":");
+	static char full_path[1024];
+
+	while (dir != NULL) {
+		
+		//strjoin path + file into fullpath
+		snprintf(full_path, sizeof(full_path), "%s/%s", path_copy, file);
+		if (check_exec(full_path)) {
+			free(path_copy);
+			return full_path;
+		}
+		//check next dir
+		dir = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return NULL;
+}
+
+void execute_echo(const char *input) {
     printf(GREEN "%s\n" RESET, input);
 }
 
-void execute_type(char *input) {
+void execute_type(const char *input) {
     if (!strncmp(input + 5, "echo", 4)) {
         printf(BLUE "echo " GREEN "is a shell builtin\n" RESET);
     } else if (!strncmp(input + 5, "exit", 4)) {
@@ -21,7 +50,11 @@ void execute_type(char *input) {
         printf(BLUE "type " GREEN "is a shell builtin\n" RESET);
     }
     else {
-        printf(RED "%s: not found\n" RESET, input + 5);
+	char* path = handle_path(input + 5);
+	if (path)
+		printf(GREEN"%s"RESET " is " YELLOW "%s\n" RESET, input + 5, path);
+	else
+        	printf(RED "%s: not found\n" RESET, input + 5);
     }
 }
 
@@ -29,7 +62,7 @@ void execute_exit() {
     exit(0);
 }
 
-void handle_command(char *input) {
+void handle_command(const char *input) {
     if (!strcmp(input, "exit 0")) {
         execute_exit();
     } else if (!strncmp(input, "echo ", 5)) {

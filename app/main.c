b154ee3abc322eaaa947c05b778ce987f4374c7e
Freitas <adavidaf2002@gmail.com>
2024-12-08 11:24:50 +0000

    added exit status

--- app/main.c
+++ app/main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
   
   char input[100];
 
-  printf("$ ");
-  fflush(stdout);
-
-  while (fgets(input, 100, stdin) != NULL) {
-	input[strlen(input) - 1] = '\0';	
-	printf("%s: not found\n", input);
+  while (1) {
+	
 	printf("$ "); 
   	fflush(stdout);
+
+	fgets(input, 100, stdin);	
+
+	input[strlen(input) - 1] = '\0';	
+	if (1) {	
+		printf("%s: not found\n", input);
+		printf("$ exit 0\n");	
+		exit(0);
+	}
   }
   return 0;
 }
